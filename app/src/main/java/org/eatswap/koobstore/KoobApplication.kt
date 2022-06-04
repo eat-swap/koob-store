28e529d45cc5abc4db720052fd41d87d04bb0583
Eat-Swap <root@eatswap.org>
2022-06-05 07:49:36 +0800

    feat: database?

---
+++ app/src/main/java/org/eatswap/koobstore/KoobApplication.kt
@@ -0,0 +1,15 @@
+package org.eatswap.koobstore
+
+import android.app.Application
+
+class KoobApplication : Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+		init()
+	}
+
+	private fun init() {
+
+	}
+}
