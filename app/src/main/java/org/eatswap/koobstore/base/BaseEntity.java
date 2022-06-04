997079027ef032fee478ae89864cfca5519c8fbf
Eat-Swap <root@eatswap.org>
2022-06-05 00:36:13 +0800

    add: user and get/set

--- app/src/main/java/org/eatswap/koobstore/base/BaseEntity.java
+++ app/src/main/java/org/eatswap/koobstore/base/BaseEntity.java
@@ -5,9 +5,17 @@ import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Index;
 
 @Entity(indexes = {
-        @Index(value = "id DESC", unique = true)
+    @Index(value = "id DESC", unique = true)
 })
 public class BaseEntity {
     @Id
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- app/src/main/java/org/eatswap/koobstore/modules/user/entity/User.java
+++ app/src/main/java/org/eatswap/koobstore/modules/user/entity/User.java
@@ -1,7 +1,28 @@
 package org.eatswap.koobstore.modules.user.entity;
 
 import org.eatswap.koobstore.base.BaseEntity;
+import org.greenrobot.greendao.annotation.NotNull;
 
 public class User extends BaseEntity {
+    @NotNull
+    private String username;
 
+    @NotNull
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
