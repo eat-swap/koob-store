ed0f0655eff968480908671bcf4b27f8bb9c7c7b
Eat-Swap <root@eatswap.org>
2022-06-05 17:35:06 +0800

    add: YES we copy code

--- app/src/main/java/org/eatswap/koobstore/modules/home/BrowseFragment.kt
+++ app/src/main/java/org/eatswap/koobstore/modules/home/BrowseFragment.kt
@@ -1,5 +1,6 @@
 package org.eatswap.koobstore.modules.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,9 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.internal.ViewUtils.dpToPx
 import org.eatswap.koobstore.R
 import org.eatswap.koobstore.databinding.FragmentBrowseBinding
 import org.eatswap.koobstore.modules.book.data.Book
+import org.eatswap.koobstore.utils.ui.GridSpacingItemDecoration
 import kotlin.random.Random
 
 class BrowseFragment : Fragment() {
@@ -22,6 +25,7 @@ class BrowseFragment : Fragment() {
 	private var _binding: FragmentBrowseBinding? = null
 	private val binding get() = _binding!!
 
+	@SuppressLint("RestrictedApi")
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
@@ -37,6 +41,7 @@ class BrowseFragment : Fragment() {
 
 		val recyclerView = v.findViewById<RecyclerView>(R.id.recycler_view_browse)!!
 		recyclerView.layoutManager = GridLayoutManager(context, 2)
+		recyclerView.addItemDecoration(GridSpacingItemDecoration(2, dpToPx(requireContext(), 10).toInt(), true))
 		recyclerView.adapter = BookAdapter(bookList!!)
 
 		return v
