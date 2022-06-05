cd5c0575e78096ded8b7c63836a1c03a97b99100
Eat-Swap <root@eatswap.org>
2022-06-05 08:23:58 +0800

    add: register?

--- app/src/main/java/org/eatswap/koobstore/modules/user/services/RegisterServices.kt
+++ app/src/main/java/org/eatswap/koobstore/modules/user/services/RegisterServices.kt
@@ -1,9 +1,16 @@
 package org.eatswap.koobstore.modules.user.services
 
-class RegisterServices {
-	companion object {
-		fun registerUser(username: String, password: String): Boolean {
-			return true
-		}
+import org.eatswap.koobstore.KoobApplication
+import org.eatswap.koobstore.modules.user.data.UserViewModel
+import org.eatswap.koobstore.modules.user.data.UserViewModelFactory
+
+class RegisterServices(private val context: KoobApplication) {
+	private val userViewModel: UserViewModel = UserViewModelFactory(
+		context.database.userDao()
+	).create(UserViewModel::class.java)
+
+	fun registerUser(username: String, password: String): Boolean {
+		userViewModel.addNewUser(username, password)
+		return true
 	}
 }
