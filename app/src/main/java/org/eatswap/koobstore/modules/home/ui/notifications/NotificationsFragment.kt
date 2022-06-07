68226667acbf882bb246f6e01b9be229caf850d0
Eat-Swap <root@eatswap.org>
2022-06-07 23:15:50 +0800

    feat: clean unused xml & class

--- app/src/main/java/org/eatswap/koobstore/modules/home/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/org/eatswap/koobstore/modules/home/ui/notifications/NotificationsFragment.kt
@@ -12,9 +12,6 @@ import org.eatswap.koobstore.databinding.FragmentNotificationsBinding
 class NotificationsFragment : Fragment() {
 
 	private var _binding: FragmentNotificationsBinding? = null
-
-	// This property is only valid between onCreateView and
-	// onDestroyView.
 	private val binding get() = _binding!!
 
 	override fun onCreateView(
@@ -22,16 +19,9 @@ class NotificationsFragment : Fragment() {
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View {
-		val notificationsViewModel =
-			ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
 		_binding = FragmentNotificationsBinding.inflate(inflater, container, false)
 		val root: View = binding.root
 
-		val textView: TextView = binding.textNotifications
-		notificationsViewModel.text.observe(viewLifecycleOwner) {
-			textView.text = it
-		}
 		return root
 	}
 
